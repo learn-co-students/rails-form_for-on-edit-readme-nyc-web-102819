bc6479018e37b027fc755d07fbba2bdb9f725224
hdp2112 <hdp2112@columbia.edu>
2019-11-24 10:37:39 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+	  	@post = Post.create(post_params)
+	  	redirect_to post_path(@post)
 	end
 
 	def edit
@@ -24,8 +21,14 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+	  	@post = Post.find(params[:id])
+	  	@post.update(post_params)
+	  	redirect_to post_path(@post)
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
 	end
 end
